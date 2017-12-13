738dceb2598b5bd71e569dfe2e1c2fc7b0172f1f
Carl A. Fahlstrom <carl.fahlstrom@gmail.com>
2017-12-13 14:51:16 -0700

    Added FT for header element in page

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVistorTest(unittest.TestCase):
@@ -20,9 +22,16 @@ class NewVistorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         """
         """
+        # User goes to the home page of the app
         self.browser.get('http://localhost:8000')
 
+        # User sees that title and header reference a todo list
         self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+
+        # A text input box is provied for adding a todo.
+
 
         # Comment this out when we are finished with all functional tests.
         self.fail("Finish writing tests.")
