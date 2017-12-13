30d8c4fa6df3b6aaa3b1f30a9d7d21fc00b889c6
Carl A. Fahlstrom <carl.fahlstrom@gmail.com>
2017-12-13 12:03:58 -0700

    Adding app for list.  added delibratily failing unit test.

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,29 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-#browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVistorTest(unittest.TestCase):
+    """
+    """
 
-assert 'Django' in browser.title
+    def setUp(self):
+        """
+        """
+        self.browser = webdriver.Chrome()
+        #self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        """
+        """
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """
+        """
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-Do', self.browser.title)
+
+        self.fail("Finish writing tests.")
+
+if __name__ == "__main__":
+    unittest.main()
