7aa85f301425bbf3c2d9aa0cec6bd456b179af67
Carl A. Fahlstrom <carl.fahlstrom@gmail.com>
2017-12-13 15:39:18 -0700

    new tests pass.  Move to templates.

--- lists/tests.py
+++ lists/tests.py
@@ -23,6 +23,13 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
+        self.assertTrue(html.startswith('<!DOCTYPE html>'))
+        self.assertIn('<html>', html)
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
+
+    def test_header_is_displayed(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<h1>', html)
